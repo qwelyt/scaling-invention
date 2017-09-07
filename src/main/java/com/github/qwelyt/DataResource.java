2b60b54e3b208636ca7696ccc10036e8e661601a
qwelyt <qwelyt@users.noreply.github.com>
2017-09-07 13:15:24 +0200

    Make a simple example work.

--- src/main/java/com/github/qwelyt/DataResource.java
+++ src/main/java/com/github/qwelyt/DataResource.java
@@ -1,15 +1,27 @@
 package com.github.qwelyt;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.Random;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 @Path("/data")
+@Produces(APPLICATION_JSON)
+@Consumes(APPLICATION_JSON)
 public class DataResource {
 
-	@Path("logged-in")
-	public Response loggedIn(){
-		return respond(String.valueOf(new Random()));
-	}
+   @GET
+   @Path("logged-in")
+   public Response loggedIn() {
+      final int i = new Random().nextInt();
+      return respond(String.valueOf(i < 0 ? i*-1 : i));
+   }
 
-
-	private Response repsond(final String str){
-		return Response.ok().entity(str).build();
-	}
+   private Response respond(final String str) {
+      return Response.ok().entity(str).build();
+   }
 }
--- src/main/java/com/github/qwelyt/ScalingInvention.java
+++ src/main/java/com/github/qwelyt/ScalingInvention.java
@@ -1,7 +1,34 @@
 package com.github.qwelyt;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
 public class ScalingInvention {
-    public static void main( String[] args ) {
-		new DataResource();
+    public static void main( final String[] args ) throws Exception{
+       new ScalingInvention().run();
     }
+
+    private void run() throws Exception{
+
+       setupServer(8080, resourceConfig());
+    }
+
+   private ResourceConfig resourceConfig() {
+
+      final ResourceConfig resourceConfig = new ResourceConfig();
+      resourceConfig.register(new DataResource());
+      return resourceConfig;
+   }
+
+   private void setupServer(final int port, final ResourceConfig resourceConfig) throws Exception {
+      final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+      context.setContextPath("/");
+      final Server jettyServer = new Server(port);
+      jettyServer.setHandler(context);
+      context.addServlet(new ServletHolder(new ServletContainer(resourceConfig)), "/*");
+      jettyServer.start();
+   }
 }
